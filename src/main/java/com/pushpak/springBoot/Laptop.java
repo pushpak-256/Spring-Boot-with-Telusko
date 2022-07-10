a4864e8b76d9fa6677b983f0ef149ef304747999
Pushpak Bhagat <pbhagat@montran.com>
2022-07-10 15:28:51 +0530

    📦 Spring Boot AutoWire

---
+++ src/main/java/com/pushpak/springBoot/Laptop.java
@@ -0,0 +1,12 @@
+package com.pushpak.springBoot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+	public void compile() {
+		System.out.println("### compiling code");
+	}
+
+}
--- src/main/java/com/pushpak/springBoot/SpringBoot1Application.java
+++ src/main/java/com/pushpak/springBoot/SpringBoot1Application.java
@@ -1,13 +1,22 @@
 package com.pushpak.springBoot;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBoot1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBoot1Application.class, args);
+		
+		//create context
+		ApplicationContext context = SpringApplication.run(SpringBoot1Application.class, args);
+		
+		Alien obj = context.getBean(Alien.class);
+		
+		obj.code();
+		
 	}
 
 }
